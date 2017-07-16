749f10eab2cc9db44298bd86eaa177cd3e2a6f09
RushFTK <867871766@qq.com>
2017-07-16 21:57:43 +0800

    Update AWBW_logcatcher(download log from website)

---
+++ tools/AWBW_LogCathcer/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace AWBW_LogCathcer
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.targetaddress_label = new System.Windows.Forms.Label();
+            this.targetaddress_textbox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.任务TToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taregtaddress_getbutton = new System.Windows.Forms.Button();
+            this.playerA_name_label = new System.Windows.Forms.Label();
+            this.playerA_alltime_hintlable = new System.Windows.Forms.Label();
+            this.playerA_alltime_label = new System.Windows.Forms.Label();
+            this.playerB_name_label = new System.Windows.Forms.Label();
+            this.帮助HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.简体中文CToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playerB_alltime_hintlable = new System.Windows.Forms.Label();
+            this.playerB_alltime_label = new System.Windows.Forms.Label();
+            this.alllog_getbutton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // targetaddress_label
+            // 
+            this.targetaddress_label.AutoSize = true;
+            this.targetaddress_label.Location = new System.Drawing.Point(12, 39);
+            this.targetaddress_label.Name = "targetaddress_label";
+            this.targetaddress_label.Size = new System.Drawing.Size(53, 12);
+            this.targetaddress_label.TabIndex = 0;
+            this.targetaddress_label.Text = "对局地址";
+            // 
+            // targetaddress_textbox
+            // 
+            this.targetaddress_textbox.Location = new System.Drawing.Point(71, 36);
+            this.targetaddress_textbox.Name = "targetaddress_textbox";
+            this.targetaddress_textbox.Size = new System.Drawing.Size(265, 21);
+            this.targetaddress_textbox.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.任务TToolStripMenuItem,
+            this.帮助HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(423, 25);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 任务TToolStripMenuItem
+            // 
+            this.任务TToolStripMenuItem.Name = "任务TToolStripMenuItem";
+            this.任务TToolStripMenuItem.Size = new System.Drawing.Size(59, 21);
+            this.任务TToolStripMenuItem.Text = "任务(&T)";
+            // 
+            // taregtaddress_getbutton
+            // 
+            this.taregtaddress_getbutton.Location = new System.Drawing.Point(342, 34);
+            this.taregtaddress_getbutton.Name = "taregtaddress_getbutton";
+            this.taregtaddress_getbutton.Size = new System.Drawing.Size(75, 23);
+            this.taregtaddress_getbutton.TabIndex = 3;
+            this.taregtaddress_getbutton.Text = "获取";
+            this.taregtaddress_getbutton.UseVisualStyleBackColor = true;
+            // 
+            // playerA_name_label
+            // 
+            this.playerA_name_label.AutoSize = true;
+            this.playerA_name_label.Location = new System.Drawing.Point(12, 71);
+            this.playerA_name_label.Name = "playerA_name_label";
+            this.playerA_name_label.Size = new System.Drawing.Size(53, 12);
+            this.playerA_name_label.TabIndex = 4;
+            this.playerA_name_label.Text = "Player_A";
+            // 
+            // playerA_alltime_hintlable
+            // 
+            this.playerA_alltime_hintlable.AutoSize = true;
+            this.playerA_alltime_hintlable.Location = new System.Drawing.Point(12, 95);
+            this.playerA_alltime_hintlable.Name = "playerA_alltime_hintlable";
+            this.playerA_alltime_hintlable.Size = new System.Drawing.Size(101, 12);
+            this.playerA_alltime_hintlable.TabIndex = 5;
+            this.playerA_alltime_hintlable.Text = "回合持续总时间：";
+            // 
+            // playerA_alltime_label
+            // 
+            this.playerA_alltime_label.AutoSize = true;
+            this.playerA_alltime_label.Location = new System.Drawing.Point(12, 118);
+            this.playerA_alltime_label.Name = "playerA_alltime_label";
+            this.playerA_alltime_label.Size = new System.Drawing.Size(119, 12);
+            this.playerA_alltime_label.TabIndex = 6;
+            this.playerA_alltime_label.Text = "00 d 00 h 00 m 00 s";
+            // 
+            // playerB_name_label
+            // 
+            this.playerB_name_label.AutoSize = true;
+            this.playerB_name_label.Location = new System.Drawing.Point(223, 71);
+            this.playerB_name_label.Name = "playerB_name_label";
+            this.playerB_name_label.Size = new System.Drawing.Size(53, 12);
+            this.playerB_name_label.TabIndex = 7;
+            this.playerB_name_label.Text = "Player_B";
+            // 
+            // 帮助HToolStripMenuItem
+            // 
+            this.帮助HToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于AToolStripMenuItem});
+            this.帮助HToolStripMenuItem.Name = "帮助HToolStripMenuItem";
+            this.帮助HToolStripMenuItem.Size = new System.Drawing.Size(61, 21);
+            this.帮助HToolStripMenuItem.Text = "帮助(&H)";
+            // 
+            // 关于AToolStripMenuItem
+            // 
+            this.关于AToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.简体中文CToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.关于AToolStripMenuItem.Name = "关于AToolStripMenuItem";
+            this.关于AToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关于AToolStripMenuItem.Text = "语言(&L)";
+            // 
+            // 简体中文CToolStripMenuItem
+            // 
+            this.简体中文CToolStripMenuItem.Name = "简体中文CToolStripMenuItem";
+            this.简体中文CToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.简体中文CToolStripMenuItem.Text = "简体中文(&C)";
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.englishToolStripMenuItem.Text = "&English";
+            // 
+            // playerB_alltime_hintlable
+            // 
+            this.playerB_alltime_hintlable.AutoSize = true;
+            this.playerB_alltime_hintlable.Location = new System.Drawing.Point(225, 95);
+            this.playerB_alltime_hintlable.Name = "playerB_alltime_hintlable";
+            this.playerB_alltime_hintlable.Size = new System.Drawing.Size(101, 12);
+            this.playerB_alltime_hintlable.TabIndex = 8;
+            this.playerB_alltime_hintlable.Text = "回合持续总时间：";
+            // 
+            // playerB_alltime_label
+            // 
+            this.playerB_alltime_label.AutoSize = true;
+            this.playerB_alltime_label.Location = new System.Drawing.Point(225, 118);
+            this.playerB_alltime_label.Name = "playerB_alltime_label";
+            this.playerB_alltime_label.Size = new System.Drawing.Size(119, 12);
+            this.playerB_alltime_label.TabIndex = 9;
+            this.playerB_alltime_label.Text = "00 d 00 h 00 m 00 s";
+            // 
+            // alllog_getbutton
+            // 
+            this.alllog_getbutton.Location = new System.Drawing.Point(14, 219);
+            this.alllog_getbutton.Name = "alllog_getbutton";
+            this.alllog_getbutton.Size = new System.Drawing.Size(117, 23);
+            this.alllog_getbutton.TabIndex = 10;
+            this.alllog_getbutton.Text = "获取全部Log数据(&L)";
+            this.alllog_getbutton.UseVisualStyleBackColor = true;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 254);
+            this.Controls.Add(this.alllog_getbutton);
+            this.Controls.Add(this.playerB_alltime_label);
+            this.Controls.Add(this.playerB_alltime_hintlable);
+            this.Controls.Add(this.playerB_name_label);
+            this.Controls.Add(this.playerA_alltime_label);
+            this.Controls.Add(this.playerA_alltime_hintlable);
+            this.Controls.Add(this.playerA_name_label);
+            this.Controls.Add(this.taregtaddress_getbutton);
+            this.Controls.Add(this.targetaddress_textbox);
+            this.Controls.Add(this.targetaddress_label);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main_Form";
+            this.Text = "AWBW_LogCatcher";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label targetaddress_label;
+        private System.Windows.Forms.TextBox targetaddress_textbox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 任务TToolStripMenuItem;
+        private System.Windows.Forms.Button taregtaddress_getbutton;
+        private System.Windows.Forms.Label playerA_name_label;
+        private System.Windows.Forms.Label playerA_alltime_hintlable;
+        private System.Windows.Forms.Label playerA_alltime_label;
+        private System.Windows.Forms.Label playerB_name_label;
+        private System.Windows.Forms.ToolStripMenuItem 帮助HToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 简体中文CToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.Label playerB_alltime_hintlable;
+        private System.Windows.Forms.Label playerB_alltime_label;
+        private System.Windows.Forms.Button alllog_getbutton;
+    }
+}
+
